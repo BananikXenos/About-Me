a52c3899056745893c26d1abd785e6f0b586f454
Kristián Kekeš <gamerix2006@gmail.com>
2022-10-04 14:56:01 +0200

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 animateTitle();
 
-function animateTitle(Title = "System.out.println(\"Hello, World!\")", delay = 300) {
+function animateTitle(Title = "About Me", delay = 300) {
     let counter = 0;
     let direction = true;
     aniTitle = setInterval(function () {
